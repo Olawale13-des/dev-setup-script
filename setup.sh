576d5c703c6329e5b44e5280ebfab61575747f04
Olawale13-des <alakosoabdulhakeemfunsho@gmail.com>
2025-11-14 17:56:27 +0100

    Add welcome banner to setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Development Environment Setup Script
+# Author: Olawale
+# Description: Automates developer environment setup
+
+echo "=================================="
+echo "  Dev Environment Setup Script"
+echo "=================================="
+echo ""
+echo "This script will help you set up your development environment."
+echo ""
